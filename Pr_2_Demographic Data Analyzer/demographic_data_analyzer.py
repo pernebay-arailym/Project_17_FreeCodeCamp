e9e918b16c7bafc0c0a4dcad69be73b48d186f6b
pernebay-arailym <pernebayarailym@gmail.com>
2024-07-03 15:45:55 +0200

    min work hours with high salary

--- Pr_2_Demographic Data Analyzer/demographic_data_analyzer.py
+++ Pr_2_Demographic Data Analyzer/demographic_data_analyzer.py
@@ -34,9 +34,9 @@ def calculate_demographic_data(print_data=True):
     min_work_hours = df['hours-per-week'].mean()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
+    num_min_workers = df[df['hours-per-week'] == 'min_work_hours']
 
-    rich_percentage = None
+    rich_percentage = round((num_min_workers['salary'] == '>50k').mean()*100, 1)
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
