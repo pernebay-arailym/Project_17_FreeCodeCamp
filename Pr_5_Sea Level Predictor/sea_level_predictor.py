31499d2920d9887e089beb8ee410bc96a6b1898b
pernebay-arailym <pernebayarailym@gmail.com>
2024-07-08 11:54:52 +0200

    cleaned

--- Pr_5_Sea Level Predictor/sea_level_predictor.py
+++ Pr_5_Sea Level Predictor/sea_level_predictor.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 
 def draw_plot():
-    df = pd.read_csv('Users/pernebayarailym/Documents/Portfolio Projects AP/Python Projects/Project_17_FreeCodeCamp/Pr_5_Sea Level Predictor/epa-sea-level.csv')
+    df = pd.read_csv('/Users/pernebayarailym/Documents/Portfolio Projects AP/Python Projects/Project_17_FreeCodeCamp/Pr_5_Sea Level Predictor/epa-sea-level.csv')
 
     # Create scatter plot
     plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='Data Points', color='blue')
@@ -39,3 +39,5 @@ def draw_plot():
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
+
+draw_plot()
