7faed1343441e86414c64234121be111d2be4d2f
pernebay-arailym <pernebayarailym@gmail.com>
2024-07-08 12:05:01 +0200

    fixed bug/failures

--- Pr_5_Sea Level Predictor/sea_level_predictor.py
+++ Pr_5_Sea Level Predictor/sea_level_predictor.py
@@ -6,7 +6,7 @@ def draw_plot():
     df = pd.read_csv('/Users/pernebayarailym/Documents/Portfolio Projects AP/Python Projects/Project_17_FreeCodeCamp/Pr_5_Sea Level Predictor/epa-sea-level.csv')
 
     # Create scatter plot
-    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='Data Points', color='blue')
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], label='Data Points', color='orange')
 
     # Create first line of best fit
     slope, intercept, r_value, p_value, std_err = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
@@ -24,16 +24,17 @@ def draw_plot():
     slope_recent, intercept_recent, r_value_recent, p_value_recent, std_err_recent=linregress(recent_df['Year'], recent_df['CSIRO Adjusted Sea Level'])
 
     #Predict sea level trough 2050 using the recent trend
-    sea_level_pred_recent=slope_recent*year_extended+intercept_recent
+    year_extended_recent = pd.Series(range(2000, 2051)) 
+    sea_level_pred_recent=slope_recent*year_extended_recent+intercept_recent
 
     #Plot the second line of the best fit
-    plt.plot(year_extended, sea_level_pred_recent, label='Best Fit Line 2000-2050', color='green')
+    plt.plot(year_extended_recent, sea_level_pred_recent, label='Best Fit Line 2000-2050', color='green')
 
 
     # Add labels and title
     plt.xlabel('Year')
-    plt.ylabel('Sea level (inches)')
-    plt.title('Rise in Sea level')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
     plt.legend()
     
     # Save plot and return data for testing (DO NOT MODIFY)
